27c7ba70d6a6312973d6791147ac185fd583e79d
akkarthikk <159886145+akkarthikk@users.noreply.github.com>
2024-03-30 23:28:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import qr from "qr-image";
 import bodyParser from "body-parser";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import fs from "fs";
 
 const app = express();
 const port = 3000;
+const currentDate = new Date().toISOString().split('T')[0];
+
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -15,14 +18,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/qr", (req, res) => {
-    res.render("qr.ejs");
-})
+  res.render("qr.ejs");
+});
+
 app.get("/generateQRCode", (req, res) => {
   const url = req.query.url;
   const qr_svg = qr.image(url, { type: 'png' });
   res.type('png');
   qr_svg.pipe(res);
+  fs.appendFile("URL.txt", url+'\n', (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
 });
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
